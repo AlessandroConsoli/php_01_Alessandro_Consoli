0cd654f4ad3ec10a6d269ed6ec350652e0e7f690
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-10-29 17:14:26 +0100

    corretti i nomi delle costanti

--- index.php
+++ index.php
@@ -10,14 +10,17 @@ var_dump($float);
 var_dump($string);
 var_dump($boolean);
 
-const INTEGER = 10;
-const FLOAT = 1.5;
-const STRING = "pippo";
-const BOOLEAN = true;
+const INTERO = 10;
+const VIRGOLA = 1.5;
+const STRINGA = "pippo";
+const BOOLEANO = true;
 
-echo INTEGER . "\n" . FLOAT . "\n" . STRING . "\n" . BOOLEAN;
+echo INTERO . "\n" . VIRGOLA . "\n" . STRINGA . "\n" . BOOLEANO;
 
-
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
 
 
 ?>
