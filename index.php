6624ee42b23a1c3cf9d7101b177a86e67b573317
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2024-10-29 17:01:01 +0100

    Selfwork PHP 1 finito

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$integer = 10;
+$float = 1.5;
+$string = "pippo";
+$boolean = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+const INTEGER = 10;
+const FLOAT = 1.5;
+const STRING = "pippo";
+const BOOLEAN = true;
+
+echo INTEGER . "\n" . FLOAT . "\n" . STRING . "\n" . BOOLEAN;
+
+
+
+
+?>
